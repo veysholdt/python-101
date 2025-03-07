3ece1790f9d40cfb7223a64ad8c9dbb9b0b1c88d
llcs <63361250+veysholdt@users.noreply.github.com>
2025-03-07 10:07:56 +0100

    Add sum and div functions

--- src/calculator.py
+++ src/calculator.py
@@ -1,2 +1,13 @@
+from typing import Iterable
+from typing import TypeVar
+
 def add(a, b):
     return a + b
+
+def my_sum(it: Iterable[int]) -> int:
+    """Return some of all items in iterable."""
+    return sum(it)
+
+def my_div(a, b):
+    """Divide two objects."""
+    return a / b
--- src/test/test_calculator.py
+++ src/test/test_calculator.py
@@ -3,7 +3,8 @@ This is the calculator test file
 """
 
 from calculator import add
-
+from calculator import my_sum
+from calculator import my_div
 
 # this is an example
 def test_add():
@@ -12,3 +13,12 @@ def test_add():
 
 
 # add your tests here
+
+def test_sum():
+    nums = [1, 2, 34, 5646, 67, 454, 2323, 34]
+    assert my_sum(nums) == sum(nums), "wrong sum :("
+
+def test_div():
+    nums = [(23,4), (23, 5), (45, 67)]
+    for pair in nums:
+        assert my_div(pair[0], pair[1]) == pair[0]/pair[1], "wrong div :("
